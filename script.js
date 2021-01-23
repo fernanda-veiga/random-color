71900806eef14d9522b2fdaa13063a64805d4ea0
Fernanda Veiga <fernandarochaveiga@gmail.com>
2021-01-22 22:12:21 -0300

    Small changes in the JS file. Give error messages

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-let color = document.querySelector("#color");
-let text = document.querySelector("#hex-code-text");
-let button = document.querySelector("#button");
+//SELECTORS
+
+//const color = document.getElementById('color');
+//const text = document.getElementById('hex-code-text');
+//const button = document.getElementById('button');
+
+
+
+//FUNCTIONS
 
 function hexCodeMaker() {
     let digitsArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
     let hexCode = "";
 
-    for (let i = 0; i <= 6 ; i++) {
+    for (let i = 0; i < 6 ; i++) {
         let randomIndex = Math.floor(Math.random() * (digitsArray.length));
         hexCode = hexCode + digitsArray[randomIndex];
     }
+
+    hexCode = "#" + hexCode;
+    document.getElementById('hex-code-text').innerHTML = "HEX Code: " + hexCode;
+    document.getElementById('color').style.backgroundColor = hexCode;
+
     return hexCode;
 }
 
-button.addEventListener("click", hexCodeMaker());
+//EVENT LISTENERS
+
+document.getElementById('button').addEventListener("click", hexCodeMaker);
